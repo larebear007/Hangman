ce1232a749530179bb7a798e95330d577bfbcbbc
larebear007 <larencozart@gmail.com>
2022-04-04 09:01:10 -0500

    Add Features: categories, play_again, multiple letter occurrence, limited attempts, one letter or whole word guessing.

--- main.py
+++ main.py
@@ -8,16 +8,16 @@ def welcome():
     time.sleep(.5)
     print('To get started, let\'s first choose a category. Your options are . . .')
     time.sleep(.5)
-    # Block below could be put in for loop if there were many more options
-    print('OPTION 1: weather')
-    time.sleep(.5)
-    print('OPTION 2: animals')
-    time.sleep(.5)
-    print('OPTION 3: drinks')
 
 
 def play_again():
-    quit()
+    restart = input('\nThat was fun! Would you like to play Hangman again? (yes or no)')
+    re = restart.lower()
+    if re == 'yes' or re == 'y':
+        play_game()
+    else:
+        print('Thanks for playing. Come again any time :)')
+        quit()
 
 
 def get_word():
@@ -25,8 +25,16 @@ def get_word():
     animal_words = ['poodle', 'zebra', 'elk', 'ocelot', 'condor', 'butterfly', 'gecko', 'rhinoceros']
     drink_words = ['chai', 'coke', 'rootbeer', 'water', 'lemonade', 'coffee', 'milk', 'sprite', 'juice']
 
-    word = None
+    # options
+    print('OPTION 1: weather')
+    time.sleep(.5)
+    print('OPTION 2: animals')
+    time.sleep(.5)
+    print('OPTION 3: drinks')
     cat = input('Which option would you like? : ').lower()
+
+    # random word assignment
+    word = None
     while True:
         if cat == 'weather':
             word = random.choice(weather_words)
@@ -50,12 +58,70 @@ def get_word():
 
 def play_game():
 
-    welcome()
     word = get_word()
 
+    x = len(word)
+    blanks = ['_'] * x
+    tries = -1
+    while True:
+        # record of word
+        print(word)
+        print('\n' + ''.join(blanks))
+
+        # finished guessing
+        if ''.join(blanks) == word:
+            print('YOU GUESSED THE WORD! \nYOU WIN! \nThe word was:', word)
+            break
+
+        # tries left
+        tries += 1
+        if tries <= 7: print('You have', 7 - tries, 'tries remaining.')
+        else:
+            print('Oh no, you are all out of tries! YOU LOSE! \nThe word was:', word)
+            break
+
+        # user input
+        entry = input('Guess a letter or the word: ')
+
+        # user guesses one letter
+        if len(entry) == 1:
+            if entry in word:
+                print('Great job! That letter is in the word.')
+                if entry in blanks:
+                    start = blanks.index(entry)
+                    pos = word.find(entry, start + 1)
+                    blanks[pos] = entry
+                else:
+                    pos = word.find(entry)
+                    blanks[pos] = entry
+            else:
+                print('No good. That letter is not in the word.')
+
+        # user guesses whole word
+        elif len(entry) > 1:
+            if entry == word:
+                print('YOU GUESSED THE WORD! YOU WIN!\nThe word was:', word)
+                break
+            else:
+                print('No good. That was not the mystery word.')
+
+        # user entry error/ blank entry
+        else:
+            print('Input error: Please enter one letter.')
 
 
 
+        # user guesses whole word
+        # if entry == word:
+        #     print('You guessed the word! \nYOU WIN! \nThe word was:', word)
+        # else:
+        #     print('Sorry that is not the word.')
 
+
+    play_again()
+
+
+
+welcome()
 play_game()
 
